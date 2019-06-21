d5b68c5c5beb280c2abdd423b9b32e3d9aeb936c
sequentialchaos <wsbraves@gmail.com>
2019-06-21 01:47:52 -0400

    add more names for hpgl export

--- lsys-editor-simple/sketch.js
+++ lsys-editor-simple/sketch.js
@@ -189,7 +189,8 @@ function colorChanged() {
 
 function buttonMousePressed() {
   let plot_txt = formatForPlotterAutoCenter(l_system.turtle.lines, width)
-  download(`hpgl_${int(random(0, 20000))}.txt`, plot_txt)
+  let words = ['cow', 'butter', 'peanut', 'cat', 'pillow', 'oven', 'meerkat', 'eskimo', 'hope', 'joy', 'oolong', 'sunset', 'stop', 'tree', 'plant', 'alpaca', 'cupcake', 'veggie', 'booksmart', 'dijsktra', 'sammet', 'hopper', 'waffle', 'puma', 'backpack', 'park', 'bridge', ]
+  download(`hpgl_${random(words)}_${int(random(0, 20000))}.txt`, plot_txt)
 
 
   // console.log("hi")
