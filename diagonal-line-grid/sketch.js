4653d503e24ce6963c2e206931ddc7c197466205
Winston Smith <wsbraves@gmail.com>
2019-09-04 04:47:25 -0400

    ^

--- diagonal-line-grid/sketch.js
+++ diagonal-line-grid/sketch.js
@@ -1,28 +1,36 @@
 function setup() {
-  // createCanvas(innerWidth, innerHeight);
-  createCanvas(800, 800);
+  min_length = min(innerWidth, innerHeight);
+  createCanvas(min_length, min_length);
   noLoop();
   colorMode(HSB, 100);
-  strokeWeight(4);
 
-  num_rows = 100;
+  num_rows = 30;
   cell_width = width / num_rows;
-  cell_height = height / num_rows;
+
+  strokeWeight(map(cell_width, 0, 30, 1, 7));
 }
 
 function draw() {
+  drawGrid();
+}
+
+function drawGrid() {
   background(3);
 
   for (let i = 0; i < num_rows; i++) {
     for (let j = 0; j < num_rows; j++) {
       x = j * cell_width;
-      y = i * cell_height;
-      stroke(map(i + j, 0, 2 * num_rows, 0, 100), 80, 80);
+      y = i * cell_width;
+      stroke(map(i + j, 0, 2 * num_rows, 0, 100), 80, 85);
       if (Math.random() < 0.5) {
-        line(x, y, x + cell_width, y + cell_height);
+        line(x, y, x + cell_width, y + cell_width);
       } else {
-        line(x + cell_width, y, x, y + cell_height);
+        line(x + cell_width, y, x, y + cell_width);
       }
     }
   }
+}
+
+function mouseClicked() {
+  redraw();
 }
